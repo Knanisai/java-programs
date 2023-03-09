da3141218867b9a93f956d8963672334d2448b4c
Knanisai <127389272+Knanisai@users.noreply.github.com>
2023-03-09 14:40:38 +0530

    addition.java

---
+++ addition.java
@@ -0,0 +1,11 @@
+class addition
+{
+	public static void main(string[] args)
+	{
+		int num 1=20,num 2=10,sum=0;
+		System.out.println("num1="+num1);
+		System.out.println("num2="+num2);
+		Sum=num 1+num 2;
+		System.out.println("The sum="+sum);
+	}
+}
